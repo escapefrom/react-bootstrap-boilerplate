e22d332adbbc9497c09c422305a201b871c1b73e
Yury Bogomolov <cleveryura@gmail.com>
2021-09-17 21:55:21 +0300

    make operation component

--- src/app/components/Account.jsx
+++ src/app/components/Account.jsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
-import { Accordion, Container, Card, ListGroup, Table, Badge } from "react-bootstrap";
+import { Accordion, Container, Card, ListGroup, Table } from "react-bootstrap";
 
 class Operation extends React.Component {
     render() {
         return (
-            <Table striped bordered hover style={{ textAlign: "center", verticalAlign: "bottom", marginTop: 4 }}>
+            <Table borderless style={{ textAlign: "center", verticalAlign: "bottom", marginTop: 4 }}>
                 <tbody>
                     <tr>
-                        <td rowspan="2" style={{ verticalAlign: "middle", width: "7%" }}>+</td>
-                        <td>2</td>
-                        <td rowspan="2" style={{ verticalAlign: "middle", width: "32%" }}>3</td>
+                        <td rowspan="2" style={{ verticalAlign: "middle", width: "7%", fontSize: "32px", fontWeight: "bold", color: this.props.type === "+" ? "green" : "red" }}>{this.props.type}</td>
+                        <td style={{textAlign: "left"}}>{this.props.category}</td>
+                        <td rowspan="2" style={{ verticalAlign: "middle", width: "32%", color: this.props.type === "+" ? "green" : "red", fontWeight: "bold", fontSize: "24px" }}>{this.props.sum} р.</td>
+                        <td rowspan="2" style={{ verticalAlign: "middle", width: "10%" }}>{this.props.time}</td>
                     </tr>
                     <tr>
-                        <td as={Badge}>4</td>
+                        <td style={{textAlign: "left", fontSize: "24px", fontWeight: "bold"}}>{this.props.target}</td>
                     </tr>
                 </tbody>
             </Table>
@@ -32,15 +33,14 @@ class Account extends React.Component {
                 {accounts.map(tab => (
                     <Accordion defaultActiveKey="0" style={{ marginBottom: 20 }}>
                         <Card>
-                            <Accordion.Toggle as={Card.Header} eventKey="0">
-                                {tab.label}
-                                <br />
-                                {tab.balance}
+                            <Accordion.Toggle as={Card.Header} eventKey="0" style={{fontSize: "18px"}}>
+                                <div style={{fontSize: "22px"}}>Счет: {tab.label}</div>
+                                <div>Баланс: {tab.balance}</div>
                             </Accordion.Toggle>
                             <Accordion.Collapse eventKey="0">
                                 <ListGroup>
-                                    <ListGroup.Item as={Operation}></ListGroup.Item>
-                                    <ListGroup.Item as={Operation}></ListGroup.Item>
+                                    <ListGroup.Item as={Operation} type="+" target="Затинацкий И.В." category="Переводы" sum="1223" time="11:21"></ListGroup.Item>
+                                    <ListGroup.Item as={Operation} type="-" target="Пятерочка" category="Супермаркеты" sum="1223" time="09:43"></ListGroup.Item>
                                 </ListGroup>
                             </Accordion.Collapse>
                         </Card>
